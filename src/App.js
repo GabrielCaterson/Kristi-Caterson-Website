3073f2c38036e3c0101009a6dc5a2f00a51b7f0d
GabrielCaterson <4404880+GabrielCaterson@users.noreply.github.com>
2023-01-12 23:58:17 -0500

    Cropping some images.

--- src/App.js
+++ src/App.js
@@ -15,11 +15,17 @@ function App() {
 
       <img className="logo-image" src={ process.env.PUBLIC_URL + "/pictures/hand.png" }></img>
 
+      <section className="art-image crop-1-box"><img className="art-image" 
+              src={ process.env.PUBLIC_URL + "/pictures/Kristi blue 1.jpg" }></img></section>
+
       <section className="three-panel"> 
+      
         <img className="art-image" 
               src={ process.env.PUBLIC_URL + "/pictures/Kristi blue 1.jpg" }></img>
-        <img className="art-image" 
+        <section className="art-image crop-1-box">
+          <img className="art-image crop-1-image" 
               src={ process.env.PUBLIC_URL + "/pictures/Kristi blue 3.jpg" }></img>
+        </section>
         <img className="art-image" 
               src={ process.env.PUBLIC_URL + "/pictures/Kristi blue 2.jpg" }></img>
       </section>
